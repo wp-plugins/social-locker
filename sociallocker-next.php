dd57826ce158276761fc862fa1f2dc67808ec48f
OnePressMedia <OnePressMedia@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-06 15:16:41 +0000

    The bug with the plost editor fixed
    git-svn-id: https://plugins.svn.wordpress.org/social-locker/trunk@676914 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sociallocker-next.php
+++ sociallocker-next.php
@@ -4,7 +4,7 @@ Plugin Name: OnePress Social Locker
 Plugin URI: http://onepress-media.com/plugin/social-locker-for-wordpress/get
 Description: Social Locker is a set of social buttons and a locker in one bottle. <strong>Give people a reason</strong> why they need to click your social buttons. Ask people to “pay” with a Like/Tweet/+1 to get access to your content, to get discount, to download, to watch a video, to view a funny picture or so. And it will help you to get more likes/tweets/+1s, traffic and customers!
 Author: OnePress
-Version: 2.0.6
+Version: 2.0.7
 Author URI: http://onepress-media.com/portfolio
 */
 
@@ -14,7 +14,7 @@ Author URI: http://onepress-media.com/portfolio
 
 require('factory/core/start.php');
 $socialLocker = factory_fr100_create_plugin(
-   __FILE__, 'sociallocker-next', '2.0.6', 'free',
+   __FILE__, 'sociallocker-next', '2.0.7', 'free',
    'http://server.onepress-media.com/api/1.0/' // the url is used to validate premium licenses, activate 
                                                // trial versions, check updates, get news and special offers
                                                // we don't collect any your date
